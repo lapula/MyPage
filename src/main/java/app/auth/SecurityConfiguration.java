936622039e940411a6941413efacbd7016bfc58a
Laur Pulliainen <pulliainen.laur@gmail.com>
2016-04-13 14:18:34 +0300

    basic css

--- src/main/java/app/auth/SecurityConfiguration.java
+++ src/main/java/app/auth/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/tervetuloa", "/uusiKayttaja").permitAll()
+                .antMatchers("/tervetuloa", "/uusiKayttaja", "/css*/**").permitAll()
                 .antMatchers(HttpMethod.GET, "/nyyttarit/*").permitAll()
                 .antMatchers(HttpMethod.POST, "/nyyttarit/*/varaa/*").permitAll()
                 .anyRequest().authenticated();
